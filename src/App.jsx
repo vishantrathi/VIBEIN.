84ca890eb86fa2f3a0af3c262921b68d9643d1a3
vishantrathi <129350236+vishantrathi@users.noreply.github.com>
2025-07-27 23:33:50 +0530

    add basic str of vite+react

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1 classname='text-4xl bg-voilet text-white'>Hello</h1>
+    <h1 className='text-4xl bg-red-500 text-white'>Hello</h1>
   )
 }
 
